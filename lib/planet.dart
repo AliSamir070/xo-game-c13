868c9cd555620e51f52ca1f9da0ff0cd174c3a91
Ali070 <Ali.zidan39@yahoo.com>
2024-12-21 21:00:10 +0200

    + add new attribute in planet

--- lib/planet.dart
+++ lib/planet.dart
@@ -3,5 +3,6 @@ class Planet{
   String signature;
   String imagePath;
   String about;
-  Planet({required this.name, required this.signature , required this.about , required this.imagePath});
+  String distanceFromEarth;
+  Planet({required this.distanceFromEarth,required this.name, required this.signature , required this.about , required this.imagePath});
 }
